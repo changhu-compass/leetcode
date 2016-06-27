28683fcba442f13c88d1b87470d5886efbe78d97
lklein <lklein@users.noreply.github.com>
2016-06-26 23:50:42 -0700

    [367. Valid Perfect Square][Accepted]committed by LeetCode Extension

---
+++ 367-Valid-Perfect-Square/solution.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPerfectSquare = function(num) {
+    var small = 1;
+    for (var big = 1; num / big > big; big *= 2) {
+        small = big;
+    }
+
+    if (num / big === big) return true;
+    if (num / small === small) return true;
+
+    var mid = Math.floor((small + big) / 2);
+    while (small < mid && mid < big) {
+        if (num / mid === mid) return true;
+        if (num / mid > mid) {
+            small = mid;
+        } else {
+            big = mid;
+        }
+        mid = Math.floor((small + big) / 2);
+    }
+
+    return false;
+};
+
