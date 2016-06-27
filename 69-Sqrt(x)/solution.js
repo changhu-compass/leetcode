984eb408627d4d8eadf8b53519942bef4f62a219
lklein <lklein@users.noreply.github.com>
2016-06-26 23:59:33 -0700

    [69. Sqrt(x)][Accepted]committed by LeetCode Extension

--- 69-Sqrt(x)/solution.js
+++ 69-Sqrt(x)/solution.js
@@ -1,7 +1,28 @@
 /**
- * @param {number} x
- * @return {number}
+ * @param {number} num
+ * @return {boolean}
  */
-var mySqrt = function(x) {
-    
+var mySqrt = function(num) {
+    if (num === 0) return 0;
+    var small = 1;
+    for (var big = 1; num / big > big; big *= 2) {
+        small = big;
+    }
+
+    if (num / big === big) return big;
+    if (num / small === small) return small;
+
+    var mid = Math.floor((small + big) / 2);
+    while (small < mid && mid < big) {
+        if (num / mid === mid) return mid;
+        if (num / mid > mid) {
+            small = mid;
+        } else {
+            big = mid;
+        }
+        mid = Math.floor((small + big) / 2);
+    }
+
+    return mid;
 };
+
